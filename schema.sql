678574d939bcdd0b116ec8548fb99edf6634a998
Gardimy Charles <gardimycharles889@gmail.com>
2023-07-17 14:15:43 -0400

    Create table animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+	id INT PRIMARY KEY,
+    name varchar(200) NOT NULL,
+	date_of_birth DATE,
+	escape_attempts INT,
+	neutered BOOLEAN,
+	weight_kg DECIMal(12,2)
 );
